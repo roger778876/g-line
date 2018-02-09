baef60f8d6589b2e685e1efb0f850f5306fd76fb
Cesar <rogercesar778876@gmail.com>
2018-02-09 10:47:24 -0500

    octant 1

--- draw.py
+++ draw.py
@@ -1,4 +1,21 @@
 from display import *
 
+def octant(x0, y0, x1, y1):
+  m = (float(y1) - y0) / (x1 - x0)
+  if ((m >= 0) & (m <= 1)):
+    return 1
+
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+  A = y1 - y0
+  B = -1 * (x1 - x0)
+  x = x0
+  y = y0
+  if (octant(x0, y0, x1, y1) == 1):
+    d = 2 * A + B
+    while (x <= x1):
+      plot(screen, color, x, y)
+      if (d > 0):
+        y += 1
+        d += 2 * B
+      x += 1
+      d += 2 * A
--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
+draw_line( 1, 1, 500, 500, screen, color )
+
+
 display(screen)
 save_extension(screen, 'img.png')
